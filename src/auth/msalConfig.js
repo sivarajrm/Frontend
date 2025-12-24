e451a162d8090bd74c09795062318394af5bf0a9
sivarajrm <158125403+sivarajrm@users.noreply.github.com>
2025-12-24 09:14:32 +0530

    Update msalConfig.js

--- src/auth/msalConfig.js
+++ src/auth/msalConfig.js
@@ -4,7 +4,7 @@ export const msalConfig = {
   auth: {
     clientId: process.env.REACT_APP_AZURE_CLIENT_ID,
     authority: `https://login.microsoftonline.com/${process.env.REACT_APP_AZURE_TENANT_ID}`,
-    redirectUri: "https://health-system24.netlify.app",
+    redirectUri: "https://health-system24.netlify.app/auth/callback",
   },
   cache: {
     cacheLocation: "localStorage",
